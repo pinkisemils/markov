e9dd06a8ac2a7907a5bf91bcd015a678a57a6e11
Emīls Piņķis <pinkisemils@gmail.com>
2017-08-12 01:49:10 +0100

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,7 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::iter::Map;
 use std::path::Path;
+use std::sync::Arc;
 use std::rc::Rc;
 use rand::{Rng, thread_rng};
 
@@ -38,7 +39,7 @@ use rand::{Rng, thread_rng};
 pub trait Chainable: Eq + Hash {}
 impl<T> Chainable for T where T: Eq + Hash {}
 
-type Token<T> = Option<Rc<T>>;
+type Token<T> = Option<Arc<T>>;
 
 /// A generic [Markov chain](https://en.wikipedia.org/wiki/Markov_chain) for almost any type. This
 /// uses HashMaps internally, and so Eq and Hash are both required.
@@ -83,7 +84,7 @@ impl<T> Chain<T> where T: Chainable {
         if tokens.is_empty() { return self }
         let mut toks = vec!(None; self.order);
         toks.extend(tokens.into_iter().map(|token| {
-            Some(Rc::new(token))
+            Some(Arc::new(token))
         }));
         toks.push(None);
         for p in toks.windows(self.order + 1) {
@@ -96,7 +97,7 @@ impl<T> Chain<T> where T: Chainable {
     /// Generates a collection of tokens from the chain. This operation is O(mn) where m is the
     /// length of the generated collection, and n is the number of possible states from a given
     /// state.
-    pub fn generate(&self) -> Vec<Rc<T>> {
+    pub fn generate(&self) -> Vec<Arc<T>> {
         let mut ret = Vec::new();
         let mut curs = vec!(None; self.order);
         loop {
@@ -113,8 +114,8 @@ impl<T> Chain<T> where T: Chainable {
     /// operation is O(mn) where m is the length of the generated collection, and n is the number
     /// of possible states from a given state. This returns an empty vector if the token is not
     /// found.
-    pub fn generate_from_token(&self, token: T) -> Vec<Rc<T>> {
-        let token = Rc::new(token);
+    pub fn generate_from_token(&self, token: T) -> Vec<Arc<T>> {
+        let token = Arc::new(token);
         if !self.map.contains_key(&vec!(Some(token.clone()); self.order)) { return Vec::new() }
         let mut ret = vec![token.clone()];
         let mut curs = vec!(Some(token.clone()); self.order);
@@ -161,7 +162,7 @@ impl Chain<String> {
     }
 
     /// Converts the output of generate(...) on a String chain to a single String.
-    fn vec_to_string(vec: Vec<Rc<String>>) -> String {
+    fn vec_to_string(vec: Vec<Arc<String>>) -> String {
         let mut ret = String::new();
         for s in &vec {
             ret.push_str(&s);
@@ -187,20 +188,20 @@ impl Chain<String> {
 
     /// Produces an infinite iterator of generated strings.
     pub fn str_iter(&self) -> InfiniteChainStringIterator {
-        let vec_to_string: fn(Vec<Rc<String>>) -> String = Chain::vec_to_string;
+        let vec_to_string: fn(Vec<Arc<String>>) -> String = Chain::vec_to_string;
         self.iter().map(vec_to_string)
     }
 
     /// Produces a sized iterator of generated strings.
     pub fn str_iter_for(&self, size: usize) -> SizedChainStringIterator {
-        let vec_to_string: fn(Vec<Rc<String>>) -> String = Chain::vec_to_string;
+        let vec_to_string: fn(Vec<Arc<String>>) -> String = Chain::vec_to_string;
         self.iter_for(size).map(vec_to_string)
     }
 }
 
 /// A sized iterator over a Markov chain of strings.
 pub type SizedChainStringIterator<'a> =
-Map<SizedChainIterator<'a, String>, fn(Vec<Rc<String>>) -> String>;
+Map<SizedChainIterator<'a, String>, fn(Vec<Arc<String>>) -> String>;
 
 /// A sized iterator over a Markov chain.
 pub struct SizedChainIterator<'a, T: Chainable + 'a> {
@@ -209,8 +210,8 @@ pub struct SizedChainIterator<'a, T: Chainable + 'a> {
 }
 
 impl<'a, T> Iterator for SizedChainIterator<'a, T> where T: Chainable + 'a {
-    type Item = Vec<Rc<T>>;
-    fn next(&mut self) -> Option<Vec<Rc<T>>> {
+    type Item = Vec<Arc<T>>;
+    fn next(&mut self) -> Option<Vec<Arc<T>>> {
         if self.size > 0 {
             self.size -= 1;
             Some(self.chain.generate())
@@ -227,7 +228,7 @@ impl<'a, T> Iterator for SizedChainIterator<'a, T> where T: Chainable + 'a {
 
 /// An infinite iterator over a Markov chain of strings.
 pub type InfiniteChainStringIterator<'a> =
-Map<InfiniteChainIterator<'a, String>, fn(Vec<Rc<String>>) -> String>;
+Map<InfiniteChainIterator<'a, String>, fn(Vec<Arc<String>>) -> String>;
 
 /// An infinite iterator over a Markov chain.
 pub struct InfiniteChainIterator<'a, T: Chainable + 'a> {
@@ -235,8 +236,8 @@ pub struct InfiniteChainIterator<'a, T: Chainable + 'a> {
 }
 
 impl<'a, T> Iterator for InfiniteChainIterator<'a, T> where T: Chainable + 'a {
-    type Item = Vec<Rc<T>>;
-    fn next(&mut self) -> Option<Vec<Rc<T>>> {
+    type Item = Vec<Arc<T>>;
+    fn next(&mut self) -> Option<Vec<Arc<T>>> {
         Some(self.chain.generate())
     }
 }
